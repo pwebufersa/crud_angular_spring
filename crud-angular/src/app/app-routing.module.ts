7eeeef70d70dedbb0abc73d13c0099fa59d343c1
PWeebUFERSA <pwebufersa@gmail.com>
2021-09-24 14:18:27 -0300

    Formatting routes.js

--- crud-angular/src/app/app-routing.module.ts
+++ crud-angular/src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  {path:'', pathMatch:'full', redirectTo: 'courses'},
-  { path: 'courses',
-  loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)}
+  { path: "", pathMatch: "full", redirectTo: "courses" },
+  {
+    path: "courses",
+    loadChildren: () =>
+      import("./courses/courses.module").then((m) => m.CoursesModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
